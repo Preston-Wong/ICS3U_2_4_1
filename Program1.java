5f46a32ef6c709a522fdab2af4a2d42f6af24200
Preston-Wong <you@example.com>
2023-10-16 09:30:33 -0400

    commented

--- Program1.java
+++ Program1.java
@@ -1,14 +1,16 @@
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description convert km to mi
+  * @author: Preton Wong
   */
   
   public void run() {
-    
+
+    // variables
     double miles;
 
+    // converting km to miles and outputting the results 
     for(int km = 10; km <= 100; km += 10){
         miles = km * 0.621371;
         System.out.println(km + "km --> " + miles + "mi");
--- Program2.java
+++ Program2.java
@@ -1,18 +1,22 @@
 class Program2 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Description calculate the amount of numbers between 1 and n
+  * @author: Preston Wong
   */
   
   public void run() {
+
+    // variables 
     int n = readInt("Enter n:" );
     int sum = 0;
 
+    // calculate the numbers from 1 to n
     for (int i = 1; i <= n; i++){
         sum = sum + i;
     }
 
+    // outputting the results
     System.out.println(sum);
   }
 }
