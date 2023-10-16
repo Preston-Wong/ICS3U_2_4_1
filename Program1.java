c8fbe7331dc6e00168133e92fa0b5e4319fe0169
Preston-Wong <you@example.com>
2023-10-16 09:26:03 -0400

    more added

--- Program1.java
+++ Program1.java
@@ -7,6 +7,12 @@ class Program1 extends ConsoleProgram {
   
   public void run() {
     
-    
+    double miles;
+
+    for(int km = 10; km <= 100; km += 10){
+        miles = km * 0.621371;
+        System.out.println(km + "km --> " + miles + "mi");
+    }
+
   }
 }
