7bb9ae8f3f63290bfd5e231849a8df792a03d540
Preston-Wong <you@example.com>
2023-10-16 09:32:53 -0400

    fixed variable names

--- Program1.java
+++ Program1.java
@@ -8,12 +8,12 @@ class Program1 extends ConsoleProgram {
   public void run() {
 
     // variables
-    double miles;
+    double dblMiles;
 
     // converting km to miles and outputting the results 
-    for(int km = 10; km <= 100; km += 10){
-        miles = km * 0.621371;
-        System.out.println(km + "km --> " + miles + "mi");
+    for(int intKm = 10; intKm <= 100; intKm += 10){
+        dblMiles = intKm * 0.621371;
+        System.out.println(intKm + "km --> " + dblMiles + "mi");
     }
 
   }
--- Program2.java
+++ Program2.java
@@ -1,22 +1,22 @@
 class Program2 extends ConsoleProgram {
 
   /**
-  * Description calculate the amount of numbers between 1 and n
+  * Description calculate the amount of numbers between 1 and the number inputted from the user 
   * @author: Preston Wong
   */
   
   public void run() {
 
     // variables 
-    int n = readInt("Enter n:" );
-    int sum = 0;
+    int intNumber = readInt("Enter n:" );
+    int intSum = 0;
 
     // calculate the numbers from 1 to n
-    for (int i = 1; i <= n; i++){
-        sum = sum + i;
+    for (int i = 1; i <= intNumber; i++){
+        intSum = intSum + i;
     }
 
     // outputting the results
-    System.out.println(sum);
+    System.out.println(intSum);
   }
 }
